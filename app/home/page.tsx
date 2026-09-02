78c133e3fadb58c12f9ad4e0cfa50d1de2d11590
LeeDS <ehdtn3540@gmail.com>
2026-09-02 22:59:04 +0900

    edit: main 페이지 소개글 수정

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
       <h1>안녕하세요 👋</h1>
-      <p>주식과 관련된 기능을 만들어보는 Toy Project 페이지입니다.</p>
+      <p>개발공부를 위한 여러가지 기능을 만들어보는 Toy Project 페이지입니다.</p>
     </main>
   );
 }
